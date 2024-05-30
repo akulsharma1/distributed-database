4a4fc9fee591385425c7c4f1450e08aaffe41fa1
akulsharma1 <akulsharma02@gmail.com>
2024-05-29 18:51:45 -0700

    started server handling with flags

--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+
+	"github.com/akulsharma1/distributed-database/internal/raft"
+)
+
+var (
+	flagNodeID = flag.Int("nodeID", -1, "node id")
+	flagPort = flag.String("port", "", "port to start node")
+)
+
 func main() {
-	
+	if *flagNodeID == -1 {
+		fmt.Println("Invalid nodeID flag")
+	}
+	if *flagPort == "" {
+		fmt.Println("Invalid port flag")
+	}
+
+	r := &raft.Raft{
+		Peers: []*raft.Raft{}, // TODO: add
+		Port: *flagPort,
+		State: raft.FOLLOWER,
+		ID: *flagNodeID,
+	}
+
+	r.StartServer()
 }
