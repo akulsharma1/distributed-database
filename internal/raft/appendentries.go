e313e842482c0d136aa2618ff73250e8fd531e51
akulsharma1 <akulsharma02@gmail.com>
2024-05-29 18:52:33 -0700

    significant progress on append entry rpc

--- internal/raft/appendentries.go
+++ internal/raft/appendentries.go
@@ -8,15 +8,25 @@ import (
 	"github.com/ybbus/jsonrpc/v3"
 )
 
+/*
+Append Entry function - leader only function.
+Sends Append Entry RPC to every follower
+*/
 func (r *Raft) CreateAndSendAppendEntry() {
 	if (r.State != LEADER) {
 		return
 	}
 
+	successCount := 0
+
 	for _, follower := range r.Peers {
 		if (follower.ID == r.ID) {
 			continue
 		}
+
+		log.Printf("[Node %v]: Sending Append Entry to follower Node %v at port %v\n", r.ID, follower.ID, follower.Port)
+
+		r.Mu.Lock()
 
 		appendEntry := AppendEntry{
 			Term: r.PersistentState.CurrentTerm,
@@ -27,6 +37,8 @@ func (r *Raft) CreateAndSendAppendEntry() {
 			LeaderCommitIndex: r.VolatileState.CommitIndex,
 			LeaderPort: r.Port,
 		}
+
+		r.Mu.Unlock()
 
 		rpcClient := jsonrpc.NewClient(fmt.Sprintf("http://localhost:%v/rpc", follower.Port))
 		resp, err := rpcClient.Call(context.Background(), "Raft.AppendEntryFollower", appendEntry)
@@ -42,8 +54,32 @@ func (r *Raft) CreateAndSendAppendEntry() {
 			continue
 		}
 
-		// TODO: add handling for response
+		r.Mu.Lock()
+
+		if !appendEntryResp.Success {
+			if appendEntryResp.Term < r.PersistentState.CurrentTerm {
+				r.PersistentState.CurrentTerm = appendEntryResp.Term
+				r.State = FOLLOWER
+				r.Mu.Unlock()
+				return
+			}
+
+			r.VolatileState.LeaderVolatileState.NextIndex[follower.ID]--
+		} else {
+			r.VolatileState.LeaderVolatileState.MatchIndex[follower.ID] = r.PersistentState.CurrentTerm
+			r.VolatileState.LeaderVolatileState.NextIndex[follower.ID] = len(r.PersistentState.Logs) - 1
+			successCount++
+		}
 		
+		r.Mu.Unlock()
+
+		
+	}
+
+	// at least half of the followers say they have added it to their logs
+	if successCount > len(r.Peers) / 2 {
+		// update commit index
+		r.VolatileState.CommitIndex = len(r.PersistentState.Logs) - 1
 	}
 }
 
@@ -55,8 +91,8 @@ func (r *Raft) AppendEntryFollower(req AppendEntry, resp *AppendEntryResp) {
 		return
 	}
 
-	r.LeaderAddr = req.LeaderPort
-	r.LeaderID = req.LeaderID
+	// r.LeaderAddr = req.LeaderPort
+	// r.LeaderID = req.LeaderID
 
 	log.Printf("[Node %v]: Received Append Entry Request/Heartbeat from Leader (Node %v)\n", r.ID, req.LeaderID)
 	
@@ -68,26 +104,59 @@ func (r *Raft) AppendEntryFollower(req AppendEntry, resp *AppendEntryResp) {
 		return
 	}
 
+	
+
 	if req.PrevLogIndex > 0 {
-		if req.PrevLogIndex > len(r.PersistentState.Logs) || r.PersistentState.Logs[req.PrevLogIndex - 1].Term != req.PrevLogTerm {
+		if req.PrevLogIndex > len(r.PersistentState.Logs) || r.PersistentState.Logs[req.PrevLogIndex].Term != req.PrevLogTerm {
 			resp.Term = r.PersistentState.CurrentTerm
 			resp.Success = false
+			r.PersistentState.Logs = r.PersistentState.Logs[:req.PrevLogIndex]
 			return
 		}
+	}
 
-		r.PersistentState.Logs = r.PersistentState.Logs[:req.PrevLogIndex]
+	/*
+	Per https://thesquareplanet.com/blog/students-guide-to-raft/
+
+	The if here is crucial. If the follower has all the entries the leader sent, the follower MUST NOT truncate its log. 
+	Any elements following the entries sent by the leader MUST be kept. This is because we could be receiving an outdated 
+	AppendEntries RPC from the leader, and truncating the log would mean “taking back” entries 
+	that we may have already told the leader that we have in our log.
+	*/
+
+	// NOTE: this code may have a bug where if logs are deleted then the database might be stale - maybe have to undo any deleted logs?
+	for i, log := range r.PersistentState.Logs[req.PrevLogIndex:] {
+		if i >= len(req.Entries) {
+			break
+		}
+		if log.Term != req.Entries[i].Term {
+			r.PersistentState.Logs = r.PersistentState.Logs[:i]
+			break
+		}
+	}
+
+
+	for _, log := range req.Entries {
+		r.HandleLog(log)
 	}
 
 	r.PersistentState.Logs = append(r.PersistentState.Logs, req.Entries...)
 
 	if req.LeaderCommitIndex > r.VolatileState.CommitIndex {
-		r.VolatileState.CommitIndex = min(req.LeaderCommitIndex, len(r.PersistentState.Logs))
+		r.VolatileState.CommitIndex = min(req.LeaderCommitIndex, len(r.PersistentState.Logs) - 1)
 	}
 
 	resp.Term = r.PersistentState.CurrentTerm
 	resp.Success = true
 }
 
-// func (r *Raft) SendAppendEntryResponse() {
-
-// }
+/*
+Database handler for logs which are appended.
+*/
+func (r *Raft) HandleLog(log Log) {
+	if log.Operation == PUT {
+		r.PersistentState.Database[log.Key] = log.Value
+	} else if log.Operation == DELETE {
+		delete(r.PersistentState.Database, log.Key)
+	}
+}
